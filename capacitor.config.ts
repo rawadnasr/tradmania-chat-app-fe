bfa1d7e38f5c04b087a04bcf13fbc1a7990a2b10
rawad <rnasr@addbloom.com>
2024-01-21 10:38:09 +0200

    updated app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'testingangularionic',
+  appName: 'TradeManiaChatAppFE',
   webDir: 'www',
   server: {
     androidScheme: 'https'
